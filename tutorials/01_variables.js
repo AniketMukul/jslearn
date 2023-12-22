1b7558432d592d2cbb626830fbd3aabbf54005e9
Aniket Mukul <96346519+AniketMukul@users.noreply.github.com>
2023-12-22 23:24:31 +0000

    variables in javascript

---
+++ tutorials/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1234 //value cant be changed
+let accountEmail = "aniketmukul13@gmail.com"
+var accountPassword = " 1234"
+let accountState
+//accountId = 2 // nod allowed
+
+let accountCity = "Jaipur"
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*Prefer not to use var
+because of issue in block scope and functional scope*/
