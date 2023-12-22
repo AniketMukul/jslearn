01e196542390121f78d57eb6372f3bec27490948
Aniket Mukul <96346519+AniketMukul@users.noreply.github.com>
2023-12-22 23:23:22 +0000

    datatype in javascript

---
+++ tutorials/02_dataType.js
@@ -0,0 +1,17 @@
+"use strict" //treat all JS code newer version
+//alert(3 + 3) //we are using nodejs,not browser
+let name = "aniket"
+let age = 22
+let isLoggedin = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+// object
+console.log(typeof undefined)
